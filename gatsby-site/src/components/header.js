0eb163d80efe63526b09c7a3856d42a5e02db945
Taylor Phillips <taylorphillips133@gmail.com>
2020-09-28 14:08:08 -0700

    add some links to the header component
    Co-authored-by: Hannah <hannah.beinstein@gmail.com>
    Co-authored-by: aesadin <aesadin@gmail.com>
    Co-authored-by: pearcy <paul@zotta.com>

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default function Header(){
   return (
-    <header>
-      <h2>This is where the header goes</h2>
+    <header className="header">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/signin">Sign In</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contactUs">Contact Us</Link></li>
+        </ul>
+        </nav>
     </header>
 
-    
   );
 }
---
+++ gatsby-site/src/pages/signin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from '../layouts/app';
+
+const SignIn = () => {
+  return (
+    <App>
+      <h1>This is where you sign up or log in</h1>
+    </App>
+  );
+
+}
+
+export default SignIn;
